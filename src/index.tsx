0d6f7f27aa98b5748ac47b333a1f10f0e1f91443
Jay R Bolton <jayrbolton@gmail.com>
2020-03-19 18:41:56 -0700

    Add a min device width of 500px to make responsive stuff easier; minor responsive fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,17 @@ import {h, render, Component} from 'preact';
 import {App} from './components/app';
 
 render(<App />, document.body);
+
+// Minimum width for the page is 500px
+// Replace device-width with 500 when the window gets small
+const metaViewport = document.querySelector('meta[name="viewport"]'); 
+window.addEventListener('resize', () => {
+    const width = window.outerWidth;
+    console.log(metaViewport.getAttribute('content'));
+    if (width <= 500) {
+        metaViewport.setAttribute('content', 'width=500');
+    } else if (metaViewport.getAttribute('content') !== 'width=device-width') {
+        console.log('setting back to device width');
+        metaViewport.setAttribute('content', 'width=device-width');
+    }
+});
