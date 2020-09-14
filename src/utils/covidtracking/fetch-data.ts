6bed7c94a05ef42645181ddf3d1813c1f3117e68
Jay R Bolton <jayrbolton@gmail.com>
2020-09-14 13:25:49 -0700

    Lint fix

--- src/utils/covidtracking/fetch-data.ts
+++ src/utils/covidtracking/fetch-data.ts
@@ -2,7 +2,7 @@ import { COVIDTRACKING_SOURCE } from "../../constants/data-sources";
 
 export async function fetchData(): Promise<string> {
   const resp = await fetch(COVIDTRACKING_SOURCE.sourceUrl, {
-    mode: 'cors',
+    mode: "cors",
   });
   const data = await resp.text();
   return data;
